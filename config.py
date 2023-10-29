aed46a797f1ee01f111e8dc81e0add92426ab2b9
Jaat Hunuman Bhakat <146306943+jaathunuman@users.noreply.github.com>
2023-10-29 08:55:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6109807345"))
 PORT = os.environ.get("PORT", "8040")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "postgres://hfnbvsco:CnjEGcBnh1CDah3IqUVCsUnvkMQHROs7@suleiman.db.elephantsql.com/hfnbvsco")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kakashimovie123:kakashimovie123@cluster0.bhdlnjb.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
